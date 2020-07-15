85efe90cc1af07b4985c2d6aed95bf37901f623b
Skyler Bluth <webdevelopmentdiva@gmail.com>
2020-07-14 23:23:47 -0700

    Updates

---
+++ precache-manifest.749ddf44f5936dfbd66b1b972dcdd208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58c2b8ba34ff54b44d0cf1bdef8e1f0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c87304292b527e689930",
+    "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "712e5559958d19da2c52",
+    "url": "/react-portfolio/static/js/2.449b857c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.449b857c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87304292b527e689930",
+    "url": "/react-portfolio/static/js/main.48220013.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "3d8d492172d30fed9cd75c9b1c945b22",
+    "url": "/react-portfolio/static/media/jumboImage.3d8d4921.jpeg"
+  }
+]);
